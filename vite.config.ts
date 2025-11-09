bb6546771b24ee90ba5db27a18f09e24aacfd5f1
yash-tile <yash.tile@indexnine.com>
2025-11-09 19:26:29 +0530

    vercel var

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH || "/Vacaka/",
+  base: process.env.NODE_ENV === "production" && process.env.VERCEL === undefined
+    ? "/Vacaka/"
+    : "/",
   plugins: [react()],
 })
