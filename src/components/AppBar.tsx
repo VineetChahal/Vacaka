014b083fd0702c58a43469b5255469eaa9039510
yash-tile <yash.tile@indexnine.com>
2025-11-09 15:54:29 +0530

    query button fixed

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -38,7 +38,7 @@ const AppBar: React.FC = () => {
               </Link>
             )
           )}
-          <Link to="/#query" className="ml-2 inline-block bg-gradient-to-r from-pink-600 to-purple-600 px-4 py-2 rounded-md text-white shadow hover:scale-105 transition-transform">
+          <Link to="/contact" className="ml-2 inline-block bg-gradient-to-r from-pink-600 to-purple-600 px-4 py-2 rounded-md text-white shadow hover:scale-105 transition-transform">
             Query
           </Link>
         </nav>
